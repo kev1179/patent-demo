ce3de49208524374d134d302476a147a04cb2de2
kev1179 <kevschool1179@gmail.com>
2025-03-14 11:29:26 -0400

    Added session storage to login page so it can remember that you are still logged in.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { TextField, Button, Typography, Link, Box, Container } from '@mui/material';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import { Navigate } from 'react-router-dom';
 import axios from "axios";
 
 const LoginPage = () => {
   const [credentials, setCredentials] = useState({ username: '', password: '' });
+  const [hasError, setHasError] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   const handleChange = (e) => {
     setCredentials({ ...credentials, [e.target.name]: e.target.value });
@@ -15,17 +19,31 @@ const LoginPage = () => {
     
     try {
       const response = await axios.post("/api/auth/login", { "username": credentials.username, "password": credentials.password });
-      console.log("Login successful:", response.data);
-      // setLoggedIn(true);
-      window.location.href = response.data.redirectUrl;
+      setSuccess(true);
     } catch (err) {
-      console.log(err);
-      console.log("Login Failed!");
-      // setError("Invalid credentials");
+      setHasError(true);
     }
 
   };
 
+  useEffect(() => {
+    const getAuthStatus = async () => {
+      try {
+        const response = await axios.get('/api/auth/getAuthStatus', { withCredentials: true });
+        if (response.data.authenticated) {
+          setSuccess(true);
+        } 
+      } catch (error) {
+        console.error('Error checking authentication:', error);
+      }
+    };
+
+    getAuthStatus();
+  }, []);
+
+  if(success)
+    return (<Navigate to = "/search"></Navigate>);
+  
   return (
     <Container component="main" maxWidth="xs" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
       <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%', border: '1px solid #ccc', borderRadius: '8px', padding: '24px' }}>
@@ -42,6 +60,7 @@ const LoginPage = () => {
             onChange={handleChange}
             autoFocus
           />
+
           <TextField
             fullWidth
             margin="normal"
@@ -51,12 +70,24 @@ const LoginPage = () => {
             value={credentials.password}
             onChange={handleChange}
           />
+
+          { hasError &&
+            <Box display="flex" alignItems="center">
+              <ErrorOutlineIcon color="error" />
+                <Typography color="error" sx={{ ml: 1, mt: 0.5}}>
+                  Incorrect username or password
+                </Typography>
+            </Box>
+          }
+
           <Link href="#" variant="body2" sx={{ display: 'block', textAlign: 'right', mt: 1 }}>
             Forgot password?
           </Link>
+
           <Button type="submit" fullWidth variant="contained" sx={{ mt: 2 }}>
             Sign In
           </Button>
+
         </Box>
       </Box>
     </Container>
