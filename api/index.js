8d3e58be61993453c997d93d1a15745312e11c91
kev1179 <kevschool1179@gmail.com>
2025-03-27 17:10:49 -0400

    Changed default port

--- api/index.js
+++ api/index.js
@@ -55,7 +55,7 @@ app.use((err, req, res, next) => {
 app.use("/patents", patentsRoutes);
 app.use("/auth", authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
